c65d5c33f3a20e4c685570e1d0ab510288782d53
Miki Yuki <fko2347073@stu/o-hara.ac.jp>
2024-04-15 10:31:04 +0900

    first

---
+++ src/main/java/team/seisaku/demo/controller/MainController.java
@@ -0,0 +1,16 @@
+package team.seisaku.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ch.qos.logback.core.model.Model;
+
+
+public class MainController
+
+{
+	@GetMapping("/")
+    public String index(Model model) {
+//      model("message", "こんにちは");
+      return "hello";
+    }
+	  }
